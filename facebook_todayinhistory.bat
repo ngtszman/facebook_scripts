322845e82adb579afdc11e1f5d2f50b074ade545
ngtszman <ngtszman1984@yahoo.com.hk>
2014-07-25 14:11:59 +0800

    Create facebook_todayinhistory.bat

---
+++ facebook_todayinhistory.bat
@@ -0,0 +1,7 @@
+call firefox https://www.facebook.com/todayinhistory?year=2013
+call firefox https://www.facebook.com/todayinhistory?year=2012
+call firefox https://www.facebook.com/todayinhistory?year=2011
+call firefox https://www.facebook.com/todayinhistory?year=2010
+call firefox https://www.facebook.com/todayinhistory?year=2009
+call firefox https://www.facebook.com/todayinhistory?year=2008
+call firefox https://www.facebook.com/todayinhistory?year=2007
