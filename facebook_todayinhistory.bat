a0acf5a62840d356cca928fe9a9d785c157a9bc1
ngtszman <ngtszman1984@yahoo.com.hk>
2016-11-09 09:31:27 +0800

    Update facebook_todayinhistory.bat

--- facebook_todayinhistory.bat
+++ facebook_todayinhistory.bat
@@ -6,6 +6,7 @@ start firefox
 timeout /t 3
 
 
+--obsolete on 2016 11 09 
 call firefox https://www.facebook.com/todayinhistory?year=2013
 call firefox https://www.facebook.com/todayinhistory?year=2012
 call firefox https://www.facebook.com/todayinhistory?year=2011
@@ -13,3 +14,6 @@ call firefox https://www.facebook.com/todayinhistory?year=2010
 call firefox https://www.facebook.com/todayinhistory?year=2009
 call firefox https://www.facebook.com/todayinhistory?year=2008
 call firefox https://www.facebook.com/todayinhistory?year=2007
+
+
+call firefox https://www.facebook.com/onthisday
